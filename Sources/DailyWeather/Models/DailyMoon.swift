09e4bbebd84309c8c6d3b07b7e27e1815a6d1baf
Johan Sögaard <johansogaard@Johans-iMac.local>
2023-10-29 15:57:33 +0100

    Moon description and icon

--- Sources/DailyWeather/Models/DailyMoon.swift
+++ Sources/DailyWeather/Models/DailyMoon.swift
@@ -39,6 +39,50 @@ public enum DailyMoonPhase {
 }
 
 public extension DailyMoonPhase {
+    var sfSymbol: String {
+        switch self {
+        case .new:
+            "moonphase.new.moon"
+        case .waxingCrescent:
+            "moonphase.waxing.crescent"
+        case .firstQuarter:
+            "moonphase.first.quarter"
+        case .waxingGibbous:
+            "moonphase.waxing.gibbous"
+        case .full:
+            "moonphase.full.moon"
+        case .waningGibbous:
+            "moonphase.waning.gibbous"
+        case .lastQuarter:
+            "moonphase.last.quarter"
+        case .waningCrescent:
+            "moonphase.waning.crescent"
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .new:
+            "New Moon"
+        case .waxingCrescent:
+            "Waxing Crescent"
+        case .firstQuarter:
+            "First Quarter"
+        case .waxingGibbous:
+            "Waxing Gibbous"
+        case .full:
+            "Full Moon"
+        case .waningGibbous:
+            "Waning Gibbous"
+        case .lastQuarter:
+            "Last Quarter"
+        case .waningCrescent:
+            "Waning Crescent"
+        }
+    }
+}
+
+public extension DailyMoonPhase {
     static func from(_ weatherKit: WeatherKit.MoonPhase) -> DailyMoonPhase {
         switch weatherKit {
         case .new: .new
